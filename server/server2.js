a460c8cf1b990239060755e9e55ea26f1d2d457a
mitchellhumphrey <mrhumphrey1s@gmail.com>
2020-11-13 20:29:08 -0600

    server 2 works

--- server/server2.js
+++ server/server2.js
@@ -1,9 +1,11 @@
-const http = require("http");
-const fs = require('fs').promises;
-
+//const fs = require('fs').promises;
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
 
 const requestListener = function (req, res) {
-    fs.readFile(__dirname + "/../client/index.html")
+    fs.readFile(__dirname + "/index.html")
         .then(contents => {
             res.setHeader("Content-Type", "text/html");
             res.writeHead(200);
@@ -14,12 +16,77 @@ const requestListener = function (req, res) {
             res.end(err);
             return;
         });
+    fs.readFile(__dirname + "/style.css")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/css");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+    
 };
+
+
+const requestListener2 = function (req, res) {
+    console.log(`${req.method} ${req.url}`);
+  
+    // parse URL
+    const parsedUrl = url.parse(req.url);
+    // extract URL path
+    let pathname = `.${parsedUrl.pathname}`;
+    // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+    const ext = path.parse(pathname).ext;
+    // maps file extention to MIME typere
+    const map = {
+      '.ico': 'image/x-icon',
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.json': 'application/json',
+      '.css': 'text/css',
+      '.png': 'image/png',
+      '.jpg': 'image/jpeg',
+      '.wav': 'audio/wav',
+      '.mp3': 'audio/mpeg',
+      '.svg': 'image/svg+xml',
+      '.pdf': 'application/pdf',
+      '.doc': 'application/msword'
+    };
+  
+    fs.exists(pathname, function (exist) {
+      if(!exist) {
+        // if the file is not found, return 404
+        res.statusCode = 404;
+        res.end(`File ${pathname} not found!`);
+        return;
+      }
+  
+      // if is a directory search for index file matching the extention
+      if (fs.statSync(pathname).isDirectory()) pathname += 'index.html' + ext;
+  
+      // read file from file system
+      fs.readFile(pathname, function(err, data){
+        if(err){
+          res.statusCode = 500;
+          res.end(`Error getting the file: ${err}.`);
+        } else {
+          // if the file is found, set Content-type and send data
+          res.setHeader('Content-type', map[ext] || 'text/html' );
+          res.end(data);
+        }
+      });
+    });
+};
+
+
 
 const host = 'localhost';
 const port = 8000;
 
-const HTMLserver = http.createServer(requestListener);
+const HTMLserver = http.createServer(requestListener2);
 HTMLserver.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
